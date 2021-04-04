247031863cf5e2b07eae06fe2356f793bb01a2eb
xudallas <52094017+xudallas@users.noreply.github.com>
2021-04-04 16:36:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def predict():
         ml_model = joblib.load(mul_reg)
         model_predcition = ml_model.predict([pred_args])
         if model_predcition == 1:
-            res = 'Affected'
+            res = '<span style = "color:#FF0000">Affected</span>'
         else:
-            res = 'Not affected'
+            res = '<span style = "color:#7AFA16">Not affected</span>'
         #return res
     return render_template('predict.html', prediction = res)
 
